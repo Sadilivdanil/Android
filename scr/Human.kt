e7a07ad99598780109602facb1c297e0c638d341
sadilov danil <sadilovdanil2006@gmail.com>
2025-10-05 22:34:47 +0700

    Update Human.kt

--- scr/Human.kt
+++ scr/Human.kt
@@ -3,7 +3,7 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.random.Random
-
+//класс человек
 class Human(
     private val fullName: String,
     private val age: Int,
@@ -11,7 +11,7 @@ class Human(
 ) : Movable {
     override var x: Double = 0.0
     override var y: Double = 0.0
-
+// метод перемещения
     override fun move(timeStep: Double) {
         val randomAngle = Random.nextDouble(0.0, 2 * PI)
         val dx = speed * timeStep * cos(randomAngle)
@@ -19,13 +19,14 @@ class Human(
         x += dx
         y += dy
     }
-
+//метод для получения имени геттер
     fun getFullName(): String = fullName
     fun getAge(): Int = age
     fun getCurrentSpeed(): Double = speed
-
+//метод для перемешения с выводом в консоль
     fun moveWithLog(timeStep: Double = 1.0) {
         move(timeStep)
         println("$fullName переместился в позицию (${"%.2f".format(x)}, ${"%.2f".format(y)})")
     }
+
 }
