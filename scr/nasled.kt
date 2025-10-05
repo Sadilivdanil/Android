24803e4362f2c4908ca6fa491338bad36aead824
sadilov danil <sadilovdanil2006@gmail.com>
2025-10-05 23:09:11 +0700

    Update nasled.kt

--- scr/nasled.kt
+++ scr/nasled.kt
@@ -9,9 +9,11 @@ class Driver(
     override fun move(timeStep: Double) {  // Убрано значение по умолчанию
         x += speed * timeStep // Движение только по X (прямолинейное)
     }
-
+    // Дополнительный метод для перемещения с логированием
+    // timeStep - шаг времени (имеет значение по умолчанию 1.0)
     fun moveWithLog(timeStep: Double = 1.0) {
         move(timeStep)
         println("ВОДИТЕЛЬ $name: (${"%.1f".format(x)}, ${"%.1f".format(y)})")
     }
 }
+
