310d8ee5fb8759d8985728b8670e75d0e8da7c24
sadilov danil <sadilovdanil2006@gmail.com>
2025-10-05 22:57:55 +0700

    Update Movable.kt

--- scr/Movable.kt
+++ scr/Movable.kt
@@ -2,8 +2,8 @@ interface Movable {
     var x: Double
     var y: Double
     val speed: Double
-
+  // Основной метод перемещения объекта
     fun move(timeStep: Double)  // Убрано значение по умолчанию
-
+// Имеет реализацию по умолчанию - можно переопределить при необходимости
     fun getPosition(): Pair<Double, Double> = Pair(x, y)
 }
